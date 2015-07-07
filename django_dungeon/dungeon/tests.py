8867d8a396805c2a55b89278c277a5dbc6761bc9
JStation <JStation@users.noreply.github.com>
2015-07-07 11:24:42 -0700

    created home.html template with corresponding unit test

--- django_dungeon/dungeon/tests.py
+++ django_dungeon/dungeon/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from dungeon.views import home_page
 
 class HomePageTest(TestCase):
@@ -7,3 +8,8 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_template(self):
+        c = Client()
+        response = c.get('/')
+        self.assertTemplateUsed(response, 'home.html')
