9084c992c2393c07c1d9198b2de997caa49cea3a
JStation <JStation@users.noreply.github.com>
2015-09-03 11:01:06 -0700

    added new functional test on adding Choices to Locations

--- django_dungeon/functional_tests/test_visitor.py
+++ django_dungeon/functional_tests/test_visitor.py
@@ -53,6 +53,62 @@ class VisitorTest(FunctionalTest):
         row_text = [item.text for item in rows]
         self.assertIn('Haunted House Mystery', row_text)
 
+    def test_can_add_choices_to_adventure(self):
+        # User opens browser and navigates to url
+        self.browser.get(self.live_server_url)
+
+        # User sees description of django dungeon below header
+        # and learns that she can create and play text adventures
+        description = self.browser.find_element_by_id("id_welcome_description")
+        self.assertIn("Create, play and share text adventures.", description.text)
+
+        # She is invited to create a new adventure
+        new_adventure_header = self.browser.find_element_by_id("id_new_adventure_header")
+        self.assertIn("Create your own adventure", new_adventure_header.text)
+
+        # She clicks the button to create a new adventure
+        with self.wait_for_page_load(timeout=10):
+            self.browser.find_element_by_id("id_new_adventure_button").click()
+
+        # This redirects her to a 'new adventure' page
+        # that invites her to enter basic story info
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertEqual("New Adventure", header.text)
+
+        # She types "Haunted House Mystery" into the "title" box
+        inputbox = self.browser.find_element_by_id('id_adventure_title')
+        inputbox.send_keys('Haunted House Mystery')
+
+        # She clicks the 'save' button to save the adventure
+        self.browser.find_element_by_id('id_save_button').click()
+
+        # And is redirected to the 'edit' adventure page with the first
+        # location of her new adventure loaded and the title of her adventure at the top
+        self.wait_for_element_with_id('id_edit_adventure_header')
+        edit_adventure_header = self.browser.find_element_by_id('id_edit_adventure_header')
+        self.assertIn("Haunted House Mystery", edit_adventure_header.text)
+
+        # the initial description has a default value explaining how to edit
+        location_description = self.browser.find_element_by_id('id_location_description')
+        default_description = "Players will begin the adventure in this room. To edit the description replace the text and click the 'save' button."
+        self.assertIn(default_description, location_description.text)
+
+        # she clicks the button below the description to add a choice
+        self.browser.find_element_by_id('id_add_choice_button').click()
+
+        # and sees a textfield appear to enter a new choice
+        self.wait_for_element_with_id('id_field_choice_1')
+        inputbox = self.browser.find_element_by_id('id_choice_field_1')
+        inputbox.send_keys('Enter Dungeon')
+
+        # she then clicks the follow button indicated by arrow icon and "save and follow" tooltip
+        with self.wait_for_page_load(timeout=10):
+            self.browser.find_element_by_id('id_follow_button_choice_1').click()
+        # and is redirected to a new room with placeholder text
+        location_description = self.browser.find_element_by_id('id_location_description')
+        default_description = "This is a new location. Edit this text and add choices as necessary."
+        self.assertIn(default_description, location_description.text)
+
 
     # Publish/Unpublish functionality
 
